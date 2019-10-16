7be2b8d6c79ad59a2bae75dcccef740e8b63a09b
Peng Du <rick.peng.du@gmail.com>
2019-10-15 17:51:56 -0700

    updates

--- class4/cmake_and_gtest/cmake_with_gtest/test/unit_tests.cpp
+++ class4/cmake_and_gtest/cmake_with_gtest/test/unit_tests.cpp
@@ -3,15 +3,22 @@
 
 TEST(example, area)
 {
-    rectangle rect {5, 3};
+    rectangle rect(5, 3);
+
+	// rect area should be equal to 15
     ASSERT_EQ(rect.area(), 15);
 }
 
 TEST(example, compare_lessthan)
 {
-    rectangle rect {5, 3};
-    rectangle larger_rec {2, 10} ;
-    rectangle smaller_rec {2, 1} ;
+    rectangle rect(5, 3);
+    rectangle larger_rec(2, 10);
+    rectangle smaller_rec(2, 1);
+
+	// rect<larger_rec should be true
+    ASSERT_TRUE(rect<larger_rec);
+
+	// rect<smaller_rec should e false
     ASSERT_TRUE(rect<larger_rec);
     ASSERT_FALSE(rect<smaller_rec);
 }
