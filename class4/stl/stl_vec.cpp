1572ea4447a6a6f429013e3cb3c5a688c7ad6686
Peng Du <rick.peng.du@gmail.com>
2019-10-15 17:36:17 -0700

    updates

---
+++ class4/stl/stl_vec.cpp
@@ -0,0 +1,35 @@
+// based on https://www.geeksforgeeks.org/ways-copy-vector-c/
+// C++ code to demonstrate STL vector 
+
+#include <vector>		// std:vector
+#include <algorithm>    // std::sort
+
+//using namespace std; 
+  
+int main() { 
+    // Initializing vector with values 
+	int arr[] = {32,71,12,45,26,80,53,33};
+	std::vector<int> vect1(arr, arr+8);
+  
+    // Declaring new vector and copying 
+    // element of old vector 
+    // constructor method, Deep copy 
+	std::vector<int> vect2(vect1); 
+  
+	printf("before sort\n");
+    for (int i=0; i<vect1.size(); i++) {
+		printf("vect1[%d] = %d\n", i, vect1[i]);
+	}
+
+	std::sort(vect1.begin(), vect1.begin()+8);
+
+	printf("\nvect1 after sort\n");
+	for (int i=0; i<vect1.size(); i++) {
+		printf("vect1[%d] = %d\n", i, vect1[i]);
+	}
+  
+	printf("\nvect2 after sort\n");
+	for (int i=0; i<vect2.size(); i++) {
+		printf("vect2[%d] = %d\n", i, vect2[i]);
+	}
+} 
