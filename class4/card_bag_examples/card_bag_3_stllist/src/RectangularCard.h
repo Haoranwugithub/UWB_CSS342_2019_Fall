d1a8b8878e2f97e7b01c7be14be183a511712eb1
Peng Du <rick.peng.du@gmail.com>
2019-10-17 15:04:48 -0700

    fix

--- class4/card_bag_examples/card_bag_3_stllist/src/RectangularCard.h
+++ class4/card_bag_examples/card_bag_3_stllist/src/RectangularCard.h
@@ -5,11 +5,11 @@
 template<class T>
 class RectangularCard : public Card<T> {
 private:
-    float m_width, m_height;
+    T m_width, m_height;
 public:
     RectangularCard() = delete;
 
-    RectangularCard(float width, float height)
+    RectangularCard(T width, T height)
             : m_width{width}, m_height{height} {};
 
     T area() const override {
