f145b126cbb0c158d59fa46eefa59efaeeab3c72
Peng Du <rick.peng.du@gmail.com>
2019-10-30 22:12:54 -0700

    updates

---
+++ class6/uniq_paths/test/unit_tests.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+#include "possible_paths.h"
+
+TEST(test_recursive, zero_one_and_two) {
+    for (int i=0; i<3; i++) {
+        ASSERT_EQ(possibleStairsRecursive(1), 1);
+    }
+}
+
+TEST(test_recursive, three) {
+    ASSERT_EQ(possibleStairsRecursive(3), 3);
+}
+
+TEST(test_recursive, four) {
+    ASSERT_EQ(possibleStairsRecursive(20), 10946);
+}
+
+TEST(test_recursive, in_class_demo ) {
+    int testSize = 30;
+    long ways = possibleStairsIterative(testSize);
+    printf("there are %ld ways for size %d\n", ways, testSize);
+}
+
+TEST(test_iterative, zero_one_and_two) {
+    for (int i=0; i<3; i++) {
+        ASSERT_EQ(possibleStairsIterative(1), 1);
+    }
+}
+
+TEST(test_iterative, four) {
+    ASSERT_EQ(possibleStairsIterative(20), 10946);
+}
