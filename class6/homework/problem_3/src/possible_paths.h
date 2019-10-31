e64f320c53a7e114c147861785d30bc780ae2d36
Peng Du <rick.peng.du@gmail.com>
2019-10-31 02:43:12 -0700

    updates

---
+++ class6/homework/problem_3/src/possible_paths.h
@@ -0,0 +1,11 @@
+long possiblePathsRecursive(int M, int N) {
+    if (M==0 || N==0) {
+        return 0;
+    }
+
+    if (M+N==2) {
+        return 1;
+    }
+
+    return possiblePathsRecursive(M-1, N) + possiblePathsRecursive(M, N-1);
+}
---
+++ class6/homework/problem_3/test/unit_tests.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+
+#include "possible_paths.h"
+
+TEST(test_recursive, zero) {
+    ASSERT_EQ(possiblePathsRecursive(0, 0), 0);
+    ASSERT_EQ(possiblePathsRecursive(0, 1), 0);
+    ASSERT_EQ(possiblePathsRecursive(0, 1), 0);
+}
+
+TEST(test_recursive, one) {
+    ASSERT_EQ(possiblePathsRecursive(1, 1), 1);
+    ASSERT_EQ(possiblePathsRecursive(1, 2), 1);
+    ASSERT_EQ(possiblePathsRecursive(2, 1), 1);
+}
+
+TEST(test_recursive, demo) {
+    ASSERT_EQ(possiblePathsRecursive(3, 2), 3);
+    ASSERT_EQ(possiblePathsRecursive(7, 3), 28);
+    ASSERT_EQ(possiblePathsRecursive(2, 4), 4);
+
+    ASSERT_EQ(possiblePathsRecursive(2, 5), 5);
+    ASSERT_EQ(possiblePathsRecursive(3, 4), 10);
+    ASSERT_EQ(possiblePathsRecursive(3, 5), 15);
+}
+
