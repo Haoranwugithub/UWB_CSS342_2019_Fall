494143ca8e6494b13eedc23a933c2ba25d9054e1
Peng Du <rick.peng.du@gmail.com>
2019-11-08 15:09:55 -0800

    update

---
+++ class7/homework/problem_1/possible_parentheses/src/possible_parentheses.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+# define MAX_SIZE 100
+
+void _possibleParenthesis(int pos, int n, int open, int close)
+{
+    static char str[MAX_SIZE];
+
+    if(close == n) {
+        printf("%s\n", str);
+        return;
+    }
+
+    if(open > close) {
+        str[pos] = '}';
+        _possibleParenthesis(pos+1, n, open, close+1);
+    }
+
+    if(open < n) {
+        str[pos] = '{';
+        _possibleParenthesis(pos+1, n, open+1, close);
+    }
+}
+
+void possibleParenthesis(int n)
+{
+    if(n > 0) {
+        _possibleParenthesis(0, n, 0, 0);
+    }
+}
+
